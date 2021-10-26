8b5ee6d6b9a7a8b3c48be37b41c1566116604767
Gilli Fjallstein <gwn787@alumni.ku.dk>
2021-10-26 11:20:12 +0200

    More comments on kernel

--- initial-code/tensor-kernels.cu.h
+++ initial-code/tensor-kernels.cu.h
@@ -1,4 +1,4 @@
-#ifndef TENSOR_KERNELS
+B#ifndef TENSOR_KERNELS
 #define TENSOR_KERNELS
 
 #define A4(X,len,I1,I2,I3,I4) (X[ (I1)*(len)*(len)*(len) + \
@@ -86,9 +86,14 @@ __global__ void tensorProdTiledKerNorm(ElTp* A, ElTp* B, ElTp* C, const int len)
         jj = (tmp / num_d) * T;
         kk = (tmp % num_d) * T;
     }
-
+    
+    //double r_t2[T_a]      // a column vector (T_a x 1)
     ElTp Areg[T];
+
+    //double r_v2[T_b]      // a row vector (1 x T_b)
     ElTp Breg[T];
+
+    //double r_t3[T_a][T_b] //register tile (T_a x T_b)
     ElTp Creg[T][T];
 
     #pragma unroll
@@ -107,9 +112,11 @@ __global__ void tensorProdTiledKerNorm(ElTp* A, ElTp* B, ElTp* C, const int len)
                 ElTp elm = 0.0;
                 bool safeA = (aa+c < len) || (ii+i < len) || (jj+j/2+q*(T/2) < len) || (dd+(j%2)*T+k < len);
                 if( safeA ) {
-                    //elm = A4(A, len, aa+c, ii+i, jj + j, dd + k); // need of LMAD copy
+                    //elm = A4(A, len, aa+c, ii+i, jj + j, dd + k); // need of LMAD copy <- Original comment
                     elm = A4(A,len,aa+c,ii+i,jj + j/2 + q*(T/2), dd + (j%2)*T + k); 
                 }
+	      //elm = g_t2[pre_t2[-] + threadIdx.x]
+	      //s_t2[treadIdx.y + j * (T_i * T_j)][threadIdx.x] = elm
                 Aloc[c][i][j/2 + q*(T/2)][(j%2)*T + k] = elm;
             }
         }
@@ -119,9 +126,11 @@ __global__ void tensorProdTiledKerNorm(ElTp* A, ElTp* B, ElTp* C, const int len)
                 ElTp elm = 0.0;
                 bool safeB = (bb+c < len) || (cc+i < len) || (kk + j/2 + q*(T/2) < len) || (dd + (j%2)*T + k < len);
                 if( safeB ) {
-                    //elm = A4(B, len, bb+c, cc+i, kk+j, dd+k); // need of LMAD copy
+                    //elm = A4(B, len, bb+c, cc+i, kk+j, dd+k); // need of LMAD copy <- Original comment
                     elm = A4(B,len,bb+c, cc+i, kk + j/2 + q*(T/2), dd + (j%2)*T + k);
                 }
+	      //elm = g_v2[pre_v2[-] + threadIdx.x]
+	      //s_v2[threadIdx.x][treadIdx.y + j * (T_k * T_c)] = elm
                 Bloc[c][i][j/2 + q*(T/2)][(j%2)*T + k] = elm;
             }
         }
@@ -133,11 +142,13 @@ __global__ void tensorProdTiledKerNorm(ElTp* A, ElTp* B, ElTp* C, const int len)
             // copy slice of A from local to register memory
             #pragma unroll
             for(int a=0; a<T; a++) {
+	      //r_t2[k] = s_t2[-][j]
                 Areg[a] = Aloc[a][i][j][d];
             }
             // copy slice of B from local to register memory
             #pragma unroll
             for(int b=0; b<T; b++) {
+	      //r_v2[k] = s_v2[j][-]
                 Breg[b] = Bloc[b][c][k][d];
             }
 
@@ -146,6 +157,7 @@ __global__ void tensorProdTiledKerNorm(ElTp* A, ElTp* B, ElTp* C, const int len)
             for(int a=0; a<T; a++) {
                 #pragma unroll
                 for(int b=0; b<T; b++) {
+		  //r_t3[i][j] += r_t2[i] * r_v2[j]
                     Creg[a][b] += Areg[a] * Breg[b];
                 }
             }
@@ -159,6 +171,7 @@ __global__ void tensorProdTiledKerNorm(ElTp* A, ElTp* B, ElTp* C, const int len)
     for(int a=0; a<T; a++) {
         #pragma unroll
         for(int b=0; b<T; b++) {
+	 // g_t3[pre_t3[-]] = r_t3[i][j]
             A6(C,len,aa+a,bb+b,cc+c,ii+i,jj+j,kk+k) = Creg[a][b];
         }
     }
